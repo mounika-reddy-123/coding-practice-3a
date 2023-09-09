7799c478773a2ebbb98eef2a12683db636d65bf7
mounika-reddy-123 <lakshmimounika.kakumuri@gmail.com>
2023-09-09 10:48:28 +0000

    recovered som mistakes

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const addDays = require("date-fns/addDays");
-function getDateAfterxDays(days) {
-  let date = new Date();
-  let dateAfterxDays = addDays(
-    new Date(date.getFullYear(), date.getMonth(), date.getDay()),
-    days
-  );
-  let resDate = `${dateAfterxDays.getDate()}-${
-    dateAfterxDays.getMonth() + 1
-  }-${dateAfterxDays.getFullYear()}`;
-  console.log(resDate);
-}
-
 const express = require("express");
 const app = express();
 
+function getDateAfterxDays(days) {
+  let date = new Date();
+  let dateAfterxDays = addDays(new Date(), days);
+  return dateAfterxDays;
+}
+dateAfterxDays = getDateAfterxDays(100);
+let resDate = `${dateAfterxDays.getDate()}-${
+  dateAfterxDays.getMonth() + 1
+}-${dateAfterxDays.getFullYear()}`;
+console.log(resDate);
+
 app.get("/", (request, response) => {
-  response.send(getDateAfterxDays(100));
+  response.send(resDate);
 });
+
+app.listen(3000);
 
 module.exports = app;
